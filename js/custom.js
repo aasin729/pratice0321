299dc8090a317e4c8e867ff2414d414b19b7685b
aasin729 <115336608+aasin729@users.noreply.github.com>
2023-03-21 14:55:50 +0900

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+﻿(function($){
+
+    $('.btnMenu').on('click', function(){
+        $('nav, section').addClass('on')
+    })
+
+    $('#gnb li a').on('click', function(){
+        let num = $(this).parent().index()
+        $('section > div').eq(num).addClass('on')
+        .siblings().removeClass('on')
+        $('nav, section').removeClass('on')
+        return false
+    })
+
+
+})(jQuery)
+
